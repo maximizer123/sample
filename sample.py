36dde369fa64a092f8a1a5b81e50b45a9992ec99
maximizer123 <maxim.jp@gmail.com>
2019-05-04 10:35:36 +0900

    setting commit

--- sample.py
+++ sample.py
@@ -1,2 +1,14 @@
-msg = 'Hello World!'
-print(msg)
+import tkinter
+
+counter = 0
+font = ("Helevetica", 32, "bold")
+button = tkinter.Button(font=font, text=str(counter))
+
+def clicked():
+    global counter, button
+    counter = counter + 1
+    button.config(text=str(counter))
+    
+button.config(command=clicked)
+button.pack()
+button.mainloop()
